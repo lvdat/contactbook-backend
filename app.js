cf5e1fb5d5805c9a488c0738511355136852d860
lvdat <lvdat13@gmail.com>
2023-02-12 17:56:55 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const contactRoutes = require('./app/routes/contact.route')
+const ApiError = require('./app/api-error')
 
 const app = express()
 
@@ -14,5 +15,18 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/contacts', contactRoutes)
+
+// handle 404 error
+app.use((req, res, next) => {
+    next(new ApiError(404, 'Resource not found'))
+})
+
+// handle errors
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error'
+    })
+})
+
 
 module.exports = app
