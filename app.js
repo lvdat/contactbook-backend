c200ce69c46e369d933181e9edba27b97fac3dd9
lvdat <lvdat13@gmail.com>
2023-02-12 17:25:35 +0700

    Cai dat express, in ra thong bao xin chao

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Welcome to Contact Book application"
+    })
+})
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./app')
+const config = require('./app/config')
+
+// start server
+const PORT = config.app.port
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+})
