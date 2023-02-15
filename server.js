19570c3a0af8f87286f5104666b236e2a1bd3237
lvdat <lvdat13@gmail.com>
2023-02-16 01:03:14 +0700

    Them va cau hinh mongodb vao du an

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const app = require('./app')
 const config = require('./app/config')
+const MongoDB = require('./app/utils/mongodb.util')
 
-// start server
-const PORT = config.app.port
+// connect to database
+async function startServer () {
+    try {
+        await MongoDB.connect(config.db.uri)
+        console.log('Connected to the database.')
+        
+        // start server
+        const PORT = config.app.port
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}.`)
+        })
+    } catch (err) {
+        console.log('Database connection failed.')
+        console.log(err)
+    }
+}
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`)
-})
+startServer();
